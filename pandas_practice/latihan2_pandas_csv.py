b4f1635611c183b0d3d48d9b8bd67ea8a39eb1a7
Mukhlis Abdillah <141989153+mukhlisabdll@users.noreply.github.com>
2024-11-18 19:13:48 +0700

    Modified latihan2_pandas_csv.py file

--- pandas_practice/latihan2_pandas_csv.py
+++ pandas_practice/latihan2_pandas_csv.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Membaca file CSV 'Salary_Data.csv' dari lokasi yang diberikan dan menyimpannya dalam sebuah DataFrame bernama 'df'
-df = pd.read_csv('C:\\data-science-practice\\datasets\\Salary_Data.csv')
+df = pd.read_csv('C:\\data_science_practice\\datasets\\Salary_Data.csv')
 
 # Menampilkan 5 baris pertama dari DataFrame 'df' menggunakan fungsi 'head()'
 print(df.head())

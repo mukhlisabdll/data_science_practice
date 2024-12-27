f345187d74f19463c9132223553ea95d2a62d207
Mukhlis Abdillah <141989153+mukhlisabdll@users.noreply.github.com>
2024-12-27 11:04:42 +0700

    Refactor and organize matplotlib, numpy, and pandas practice files

---
+++ python-basics/variables/book_discount.py
@@ -0,0 +1,10 @@
+judul_buku = "Madilog"
+harga = 140000
+diskon = 10
+
+harga_setelah_diskon = harga - (harga * diskon / 100)
+
+print(f"Buku \"{judul_buku}\" setelah diskon 10% menjadi seharga Rp{harga_setelah_diskon:.2f}.")
+
+
+

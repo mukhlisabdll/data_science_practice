9fb3a6d44a220a6416ed38b7a8be85423249afd4
Mukhlis Abdillah <141989153+mukhlisabdll@users.noreply.github.com>
2024-12-31 09:54:58 +0700

    Add folder if-else

---
+++ python-basics/if-else/number_check.py
@@ -0,0 +1,10 @@
+# Menentukan apakah sebuah angka positif, nol, atau negatif
+angka = int(input("Masukan angka: "))
+
+# Mengevaluasi apakah nilai positif, nol, atau negatif
+if angka > 0:
+    print("Angka positif")
+elif angka == 0:
+    print("Angka nol")
+else:
+    print("Angka negatif")

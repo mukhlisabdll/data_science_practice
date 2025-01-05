3177dc1815c4daca3be69feb75c1cbb33778234a
Mukhlis Abdillah <141989153+mukhlisabdll@users.noreply.github.com>
2025-01-05 23:56:36 +0700

    Removed unused pandas practice files

--- pandas_practice/pandas_csv.py
+++
@@ -1,9 +0,0 @@
-# Mengimpor library pandas dengan alias 'pd'
-import pandas as pd
-
-# Membaca file CSV 'Salary_Data.csv' dari lokasi yang diberikan dan menyimpannya dalam sebuah DataFrame bernama 'df'
-df = pd.read_csv('C:\\data_science_practice\\datasets\\Salary_Data.csv')
-
-# Menampilkan 5 baris pertama dari DataFrame 'df' menggunakan fungsi 'head()'
-print(df.head())
-
--- python_basics/control_flow/main.py
+++ python_basics/control_flow/main.py
@@ -1,10 +1,15 @@
-# Menentukan apakah sebuah angka positif, nol, atau negatif
+# Meminta pengguna untuk memasukkan angka dan mengubahnya menjadi integer
 angka = int(input("Masukan angka: "))
 
-# Mengevaluasi apakah nilai positif, nol, atau negatif
+# Mengecek apakah angka yang dimasukkan lebih besar dari 0
 if angka > 0:
+    # Jika angka lebih besar dari 0, maka angka tersebut positif
     print("Angka positif")
+# Mengecek apakah angka yang dimasukkan sama dengan 0
 elif angka == 0:
+    # Jika angka sama dengan 0, maka angka tersebut adalah nol
     print("Angka nol")
+# Jika angka tidak lebih besar dari 0 dan tidak sama dengan 0, maka angka tersebut negatif
 else:
+    # Jika angka lebih kecil dari 0, maka angka tersebut negatif
     print("Angka negatif")
